9d67bb2d75edda5423dde51575e7ea6929410594
karol <kkraczyna@gmai.com>
2019-07-06 13:13:05 +0200

    new idea

--- SPI_frame.h
+++ SPI_frame.h
@@ -2,7 +2,7 @@
 
 typedef struct 	// to STM
 {
-	char mode_of_frame = 1;
+	char frame_id = 1;
 
 	float pos_x;
 	float pos_y;
@@ -11,28 +11,12 @@ typedef struct 	// to STM
 	float pos_roll;
 	float pos_yaw;
 
-	bool module1_power_switch : 1;
-	bool module2_power_switch : 2;
-	bool module4_power_switch : 3;
-	bool module3_power_switch : 4;
-	bool module5_power_switch : 5;
-	bool module6_power_switch : 6;
-	bool module7_power_switch : 7;
-	bool module8_power_switch : 8;
-
 	char meaning_of_life = 42;
-} SPIFrame_from_rpi;
+} SPIFrame_from_rpi_data;
 
 typedef struct 	// to STM
 {
-	char mode_of_frame = 2;
-	
-	float pos_x;
-	float pos_y;
-	float pos_z;
-	float pos_pitch;
-	float pos_roll;
-	float pos_yaw;
+	char frame_id = 2;
 
 	bool module1_power_switch : 1;
 	bool module2_power_switch : 2;
@@ -51,11 +35,11 @@ typedef struct 	// to STM
 	float PID6_P, PID6_I, PID6_D;
 
 	char meaning_of_life = 42;
-} SPIFrame_from_rpi_extended;
+} SPIFrame_from_rpi_settings;
 
 typedef struct 	// to rpi
 {
-	char mode_of_frame = 1;
+	char frame_id = 1;
 
 	float pos_x;
 	float pos_y;
